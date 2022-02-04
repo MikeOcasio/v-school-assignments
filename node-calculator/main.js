176bc55eccdce19eae4f3dd3733c789ff04a9537
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-03 22:14:36 -0500

    Addition: added readline-sync, added variables to identify the intergers in the calculator.

--- node-calculator/main.js
+++ node-calculator/main.js
@@ -13,3 +13,12 @@ function mul (num1, num2) {
 function div (num1, num2) {
     
 }
+
+var readLineSync = require("readline-sync");
+
+var num1 = readLineSync.question ("Enter first number: ")
+console.log(num1);
+
+var num2 = readLineSync.question ("Enter second number: ")
+console.log(num2);
+
