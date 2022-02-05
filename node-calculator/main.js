7b01fa1fe6ebd83f2ab9f25154755576af9be9e5
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-04 22:20:44 -0500

    Addition: main.js
    Added the operators to the functions and added a switch case that allows the user input within the terminal to select a specific operator.

--- node-calculator/main.js
+++ node-calculator/main.js
@@ -1,24 +1,54 @@
-function add (num1, num2) {
-
-}
-
-function sub (num1, num2) {
-
-}
-
-function mul (num1, num2) {
-
-}
-
-function div (num1, num2) {
-    
-}
-
 var readLineSync = require("readline-sync");
 
-var num1 = readLineSync.question ("Enter first number: ")
-console.log(num1);
+function add(num1, num2) {
+  return Number(num1) + Number(num2);
+}
 
-var num2 = readLineSync.question ("Enter second number: ")
-console.log(num2);
+function sub(num1, num2) {
+  return Number(num1) - Number(num2);
+}
 
+function mul(num1, num2) {
+  return Number(num1) * Number(num2);
+}
+
+function div(num1, num2) {
+  return Number(num1) / Number(num2);
+}
+
+var user = readLineSync.question("Hello, what is your name? ");
+console.log(`Hey there, ${user} welcome to node-calculator.`);
+
+var number1 = readLineSync.question("Enter first number: ");
+console.log(`Your first number is: ${number1}`);
+
+var number2 = readLineSync.question("Enter second number: ");
+console.log(`Your second number is: ${number2}`);
+
+function operatorChoice() {
+  let operator = readLineSync.question(`Select operator: `, {
+    require: string,
+  });
+  switch (operator) {
+    case "+":
+      console.log(add(number1, number2));
+
+      return operator;
+    case "-":
+      console.log(sub(number1, number2));
+
+      return operator;
+    case "*":
+      console.log(mul(number1, number2));
+
+      return operator;
+    case "/":
+      console.log(div(number1, number2));
+
+      return operator;
+    default:
+      console.log("Please enter '+', '-', '*', or '/'!");
+  }
+}
+
+operatorChoice();
