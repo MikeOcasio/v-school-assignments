d6ff2d07d7e9c8d66ee29e07f0a460d43840f618
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-13 20:15:14 -0500

    Initial Commit
    - Initial Commit for loop and array practice

---
+++ loop-array-practice/index.js
@@ -0,0 +1,50 @@
+var officeItems = [
+  "stapler",
+  "monitor",
+  "computer",
+  "desk",
+  "lamp",
+  "computer",
+  "lamp",
+  "stapler",
+  "computer",
+  "computer",
+];
+
+for (i = 0; i <= officeItems.length; i++){
+
+    if (officeItems[i] === "computer"){
+        console.log()
+    }
+
+};
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+    name: "Mike",
+    age: 12,
+    gender: "male",
+  },
+  {
+    name: "Madeline",
+    age: 80,
+    gender: "female",
+  },
+  {
+    name: "Cheryl",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "Sam",
+    age: 30,
+    gender: "male",
+  },
+  {
+    name: "Suzy",
+    age: 4,
+    gender: "female",
+  },
+];
+
+
