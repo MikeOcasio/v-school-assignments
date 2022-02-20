05c196ce947a2d8d8b3aef66c6b6c4b93af8e1a2
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-20 17:06:10 -0500

    Initial commit and completion
    String Methods exercise. Addition of functions that use methods for the "string" class

---
+++ string-methd-excrs/index.js
@@ -0,0 +1,38 @@
+console.log(
+  "---------------------------toUpper/toLower--------------------------------------"
+);
+
+function lowerUpper(thisStr, string) {
+  let strUpper = string.toUpperCase();
+  let strLower = thisStr.toLowerCase();
+
+  return console.log(strUpper + strLower);
+}
+lowerUpper("HELLO", "hello");
+
+console.log(
+  "-----------------------------------Math.floor------------------------------"
+);
+
+function findMidIndex(phrase, phraseRecip) {
+  let mid = phrase.length / 2;
+  let mid2 = phraseRecip.length / 2;
+  console.log("hello mid index is: " + Math.floor(mid));
+  console.log("hello world mid index is: " + Math.floor(mid2));
+}
+
+findMidIndex("Hello", "Hello World");
+
+console.log(
+  "------------------------------Slice-----------------------------------"
+);
+
+function firstHalf(myString, myString2) {
+  let halfString = myString.slice(0, 2); //half of "hello" is 2 according to our findMid so start and index 0 and stop at index 2 it will cut off index2-the end of the array.
+
+  let halfString2 = myString2.slice(0, 5); //half of "hello world" is 5 according to our findMid so start and index 0 and stop at index 5 it will cut off index2-the end of the array.
+  console.log(halfString);
+  console.log(halfString2);
+}
+
+firstHalf("Hello", "Hello World");
