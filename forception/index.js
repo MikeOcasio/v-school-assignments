3227538acda15110c53562904a7b8b122a5c6258
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-20 16:57:55 -0500

    Update: Index.js
    - Added nested for loop.
    - Added return for forception function

--- forception/index.js
+++ forception/index.js
@@ -1,23 +1,18 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+var updatedArr = [];
 
-var alphArrUpprCase = alphabet.toUpperCase();
-
-var alphArrSplit = alphArrUpprCase.split("");
-
-var value = 0;
-
-console.log(alphArrSplit);
-
- for (i = 0; i < people.length; i++){
-
-    
-
- }
-
-function forception(people, alphabet) {
-  for (i = 0; i < people.length; i++) {
-    console.log(people);
+function forception() {
+  for (var i = 0; i < people.length; i++) {
+    // This creates a new array and pushes the names plus a ":"
+    updatedArr.push(people[i] + ":");
+    for (var i = 0; i < alphabet.length; i++) {
+      // This loops through the new alphabet and pushes each letter to the new array
+      updatedArr.push(alphabet[i].toUpperCase());
+    }
+    console.log(updatedArr);
   }
+  return updatedArr;
 }
+forception();
