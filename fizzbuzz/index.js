b0423c9792e45e7b4d64b855438b1011ddfc0f32
Michael Ocasio <ocasio.michael96@gmail.com>
2022-02-27 15:47:29 -0500

    Update: Addition of read-line sync
    - Completed fizzbuzz
    -Added Node.js and readline-sync to colossal-adv-rpg

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,6 +1,14 @@
 for (let i = 0; i <= 100; i++) {
-    let output = [];
+  let output = [];
 
-    //all numbers divisible by 3 &&/ or all numbers divisible by 5 that both have a remainder of 0, result= "FizzBuzz"
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(output += "FizzBuzz");
+  //all numbers divisible by 3 &&/ or all numbers divisible by 5 that both have a remainder of 0, result= "FizzBuzz"
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log((output += "FizzBuzz"));
+  } else if (i % 3 === 0) {
+    console.log((output += "Fizz"));
+    //all numbers that are only divisible by 5 and not 3, result="Buzz"
+  } else if (i % 5 == 0) {
+    console.log((output += "Buzz"));
+    //else is a catch all for all reamaing numbers that are not divisible by 5 or 3 in which case we print the number in that index.
+  } else console.log((output = i));
+};
