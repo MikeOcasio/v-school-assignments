f150fb4dfc272fc95a6b730af63c153f5f7f2998
Michael Ocasio <ocasio.michael96@gmail.com>
2022-03-10 20:23:33 -0500

    Addtion: JS
    -Added Js to give functionality to the HTML sheet

--- Level 2/simple-calculator/index.js
+++ Level 2/simple-calculator/index.js
@@ -1,12 +1,42 @@
-"use_strict";
+let form = document["smplCalculator"];
 
-const form = document["add"]
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault()
-
-    const number1 = form.add.value
-
-    console.log(number1)
-    
-})
+const addResult = form.querySelector("#add").querySelector("#addResult");
+console.log(addResult);
+addResult.addEventListener("click", function (event) {
+  event.preventDefault();
+  let num1 = form.num1.value;
+  let num2 = form.num2.value;
+  let addSum = Number(num1) + Number(num2);
+  let li = document.getElementById("addd");
+  console.log(li);
+  li.textContent = `${num1}` + " + " + `${num2}` + " = " + `${addSum}`;
+  console.log(addSum);
+});
+const subResult = form
+  .querySelector("#sub")
+  .querySelector("#subResult");
+console.log(subResult);
+subResult.addEventListener("click", function (event) {
+  event.preventDefault();
+  let num3 = form.num3.value;
+  let num4 = form.num4.value;
+  let subSum = Number(num3) - Number(num4);
+  let li = document.getElementById("subtract");
+  console.log(li);
+  li.textContent = `${num3}` + " - " + `${num4}` + " = " + `${subSum}`;
+  console.log(subSum);
+});
+const multiResult = form
+  .querySelector("#mul")
+  .querySelector("#multiResult");
+console.log(multiResult);
+multiResult.addEventListener("click", function (event) {
+  event.preventDefault();
+  let num5 = form.num5.value;
+  let num6 = form.num6.value;
+  let mulSum = Number(num5) * Number(num6);
+  let li = document.getElementById("multiply");
+  console.log(li);
+  li.textContent = `${num5}` + " * " + `${num6}` + " = " + `${mulSum}`;
+  console.log(mulSum);
+});
