255601298defe5d083a6ee1ba25ce3c66ab1ba06
Michael Ocasio <ocasio.michael96@gmail.com>
2022-03-18 21:42:19 -0400

    Update: Index.js
    -Added get total function, along with listeners

---
+++ Level 2/mario-pest-control/index.js
@@ -0,0 +1,22 @@
+function getTotal() {
+  let incValueTypeG = 5;
+  let incValueTypeB = 7;
+  let incValueTypeC = 11;
+
+  return (
+    document.getElementById("G-total").value * incValueTypeG +
+    document.getElementById("BB-total").value * incValueTypeB +
+    document.getElementById("CC-total").value * incValueTypeC
+  );
+}
+console.log(getTotal);
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  let result = (document.getElementById("total").value =
+    "Your Price is " + getTotal() + " Coins");
+  console.log(result);
+  console.log(goomba.gTotal.value);
+  console.log(cheapCheep.cTotal.value);
+  console.log(bobOmbs.bTotal.value);
+});
+getTotal();
