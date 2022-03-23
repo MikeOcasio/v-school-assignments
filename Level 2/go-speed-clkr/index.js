c4833c2949aa9b0e8832c0d28e284aad4a6e1746
Michael Ocasio <ocasio.michael96@gmail.com>
2022-03-22 21:00:51 -0400

    Update: speed-clikr
    - Updated speed clicker by adding the src code

---
+++ Level 2/go-speed-clkr/index.js
@@ -0,0 +1,30 @@
+const setCount = document.getElementById("count");
+setCount.textContent = localStorage.count || 0;
+
+let counter = localStorage.count || 0;
+
+function clickCount() {
+  document.getElementById("count").addEventListener("click", function () {
+    counter++;
+    setCount.textContent = counter;
+    localStorage.setItem("count", counter);
+    console.log(counter);
+  });
+}
+clickCount();
+
+var clickClock = 10;
+var downloadTimer = setInterval(function () {
+  if (clickClock <= 0) {
+    clearInterval(downloadTimer);
+    let endRace = (document.getElementById("timer").innerHTML = "Times Up");
+    alert(`${endRace}, Race completed with ${counter} clicks`);
+    localStorage.removeItem("count");
+    counter = 0;
+    setCount.textContent = 0;
+  } else {
+    document.getElementById("timer").innerHTML =
+      clickClock + " seconds remaining";
+  }
+  clickClock -= 1;
+}, 1000);
