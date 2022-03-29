16f4961514c29169602154597e179f69fccc1684
Michael Ocasio <ocasio.michael96@gmail.com>
2022-03-28 20:17:28 -0400

    Update index.js
    - Added JS code to index.js

---
+++ Level 2/array-map-excer/index.js
@@ -0,0 +1,129 @@
+function doubleNumbers(arr) {
+  return arr.map(function (num) {
+    return num * 2;
+  });
+}
+console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+/* Take an array of numbers and make them strings */
+function stringItUp(arr) {
+  return arr.map(function (num) {
+    return num.toString();
+  });
+}
+
+console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+
+/* Capitalize each of an array of names */
+function capitalizeNames(arr) {
+  return arr.map(function (name) {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  });
+}
+
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+
+/* Make an array of strings of the names */
+
+function namesOnly(arr) {
+  return arr.map(function (object) {
+    return object.name;
+  });
+}
+
+console.log(
+  namesOnly([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
+// ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+/* Make an array of strings of the names saying whether or not they can go to the Matrix */
+
+function makeStrings(arr) {
+  return arr.map(function (person) {
+    if (person.age > 18)
+      return `${person.name} is an adult they can enter the Matrix `;
+    else return `${person.name} is under age, too young to enter the Matrix`;
+  });
+}
+
+console.log(
+  makeStrings([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
+// ["Angelina Jolie can go to The Matrix",
+// "Eric Jones is under age!!",
+// "Paris Hilton is under age!!",
+// "Kayne West is under age!!",
+// "Bob Ziroll can go to The Matrix"]
+
+/* Make an array of the names in h1s, and the ages in h2s */
+function readyToPutInTheDOM(arr) {
+  return arr.map(function (person) {
+    return `<h1>${person.name}</h1> <h2>${person.age}</h2>`;
+  });
+}
+console.log(
+  readyToPutInTheDOM([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
