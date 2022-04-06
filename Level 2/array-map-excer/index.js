6998d61f1926f580d8683b70f99c125620704fec
Michael Ocasio <ocasio.michael96@gmail.com>
2022-04-05 21:01:23 -0400

    Debug: Function
    - Debugged small issue with "capitalizeName" function

--- Level 2/array-map-excer/index.js
+++ Level 2/array-map-excer/index.js
@@ -12,8 +12,7 @@ console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
 
 /* Capitalize each of an array of names */
 function capitalizeNames(arr) {
-  return arr.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-);
+  return arr.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
 }
 
 console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
