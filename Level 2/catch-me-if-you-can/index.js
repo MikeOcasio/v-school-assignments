21a6b05038f0b5e2c1eeb07cb810abacdec635e9
Michael Ocasio <ocasio.michael96@gmail.com>
2022-04-15 22:42:25 -0400

    Addition: Initial Commit
    -Error handling assignment Catch me if you can initialized

---
+++ Level 2/catch-me-if-you-can/index.js
@@ -0,0 +1,10 @@
+function sum(x, y) {
+  //check data types first and throw error
+  return x + y;
+}
+
+
+var user = { username: "sam", password: "123abc" };
+function login(username, password) {
+  //check credentials
+}
