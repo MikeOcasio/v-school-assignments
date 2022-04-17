c5bce4b6cfd74ea66caab0c6871146483f4d8503
Michael Ocasio <ocasio.michael96@gmail.com>
2022-04-17 15:36:33 -0400

    Addition: Node.JS & Tailwind
    -Added the dependencies to used both node.js and tailwind for the styling

--- Level 2/catch-me-if-you-can/index.js
+++ Level 2/catch-me-if-you-can/index.js
@@ -1,7 +1,22 @@
+
+sum("", "");
+
 function sum(x, y) {
   //check data types first and throw error
+  try {
+    if (x && y === NaN) {
+
+      throw new Error("One or more inputs is not a number")
+    }
+  }
+
+  catch(err){
+    console.log(err)
+  }
   return x + y;
 }
+
+
 
 
 var user = { username: "sam", password: "123abc" };
---
+++ Level 2/zen-garden/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./layuots/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
